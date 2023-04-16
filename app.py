c841fbf8b416ac00832b545b5a7a770366def6a5
Whitewalker-459 <130918267+Whitewalker-4596@users.noreply.github.com>
2023-04-16 16:26:44 +0000

    html added

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app =Flask(__name__)
 
 @app.route("/")
 def hell0_world():
-  return "hell0_world"
+  return render_template("home.html")
 
 
 
